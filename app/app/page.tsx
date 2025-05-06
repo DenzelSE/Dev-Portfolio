733492025a4932226d75708aa6f4bcbc339a7add
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-05-06 03:28:14 +0200

    components and main page layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
+import Hero from "@/components/hero"
+import Navbar from "@/components/navbar"
+import About from "@/components/about"
+import Skills from "@/components/skills"
+import Projects from "@/components/projects"
+import Contact from "@/components/contact"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
-    <p>DEnzel Portfolio</p>
-  );
+    <main className="min-h-screen bg-[#0f172a]">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  )
 }
